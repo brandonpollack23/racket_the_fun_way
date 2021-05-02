15a873b196936ff9cbd526ac2fb7fe710573a308
Brandon Pollack <brandonpollack23@gmail.com>
2021-05-02 01:02:01 -0400

    Made generic not global

--- insurance.rkt
+++ insurance.rkt
@@ -1,19 +1,22 @@
 #lang racket
 (require math/number-theory)
 
-(define triples '())
-(define (gen-triples d1)
-  (let* ([q (/ 36 d1)]
-         [divs (divisors q)])
-    (define (try-div divs)
-      (when (not (null? divs))
-        (let* ([d2 (car divs)]
-               [d3 (/ q d2)])
-          (set! triples (cons `(,d3 ,d2 ,d1) triples)))
-        (try-div (cdr divs))))
-    (try-div divs)))
+(define (gen-triples prod)
+  (define (gen-triples/helper d1 triples)
+    (let* ([q (/ prod d1)]
+           [divs (divisors q)])
+      (define (try-div divs triples)
+        (if (not (null? divs))
+            (let* ([d2 (car divs)]
+                 [d3 (/ q d2)]
+                 [new-triples (cons `(,d3 ,d2 ,d1) triples)])
+              (try-div (cdr divs) new-triples))
+            triples))
+      (try-div divs '())))
+  (apply append (for/list ([d (divisors prod)]) (gen-triples/helper d '()))))
 
-(for ([d (divisors 36)]) (gen-triples d))
+(define triples (gen-triples 36))
+  
 
 ;; (for ([triple triples]) (printf "~a: ~a\n" triple (apply + triple)))
 
